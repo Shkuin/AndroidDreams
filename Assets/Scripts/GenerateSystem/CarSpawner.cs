fdfd5d15681d921d434dc934bd887f8733202943
Artem Melentev <tema2208st@mail.ru>
2022-10-03 23:05:20 +0700

    Also delete extra files

--- Assets/Scripts/GenerateSystem/CarSpawner.cs
+++ Assets/Scripts/GenerateSystem/CarSpawner.cs
@@ -19,7 +19,6 @@ public class CarSpawner : MonoBehaviour
     private float _generatedCarHeight;
     private float _nextSpawn = 1.0f;
     private float _mainCameraWidth;
-    //private List<float> _yCoordinateList = new List<float>();
 
     void Start()
     {
@@ -30,7 +29,6 @@ public class CarSpawner : MonoBehaviour
         // ------
         float y = Random.Range(_generatedCarHeight / 5, _mainCameraHeight - _generatedCarHeight / 2);
         _spawnGameObject = Instantiate(_leftMoveCarsObj, new Vector2(_mainCameraWidth, y), Quaternion.identity);
-        //_yCoordinateList.Add(y);
     }
 
     private float SetYCooridinateWithConstr()
@@ -41,7 +39,6 @@ public class CarSpawner : MonoBehaviour
         {
             randY = Random.Range(_generatedCarHeight / 5, _mainCameraHeight - _generatedCarHeight / 2) * signArray[Random.Range(0,3)];
         }
-        //_yCoordinateList.Add(randY);
         return randY;
     }
 
@@ -59,30 +56,10 @@ public class CarSpawner : MonoBehaviour
 
     private bool CheckConstrainForSpawnPoint(float coordinate)
     {
-        //bool constrain = false;
-        //for (int i = 0; i < _yCoordinateList.Count; i++)
-        //{
-        //    if (!constrain)
-        //    {
-        //        constrain = (coordinate >= _yCoordinateList[i] - _mainCarHeight * 2) &
-        //                    (coordinate <= _yCoordinateList[i] + _mainCarHeight * 2);
-        //    }
-        //}
         return (coordinate >= -_generatedCarHeight / 2 & coordinate <= _generatedCarHeight / 2) |
                (coordinate <= -_mainCameraHeight + _generatedCarHeight / 2 |
                 coordinate >= _mainCameraHeight - _generatedCarHeight / 2) |
                (coordinate <= _spawnGameObject.transform.position.y + _mainCarHeight &
                 coordinate >= _spawnGameObject.transform.position.y - _mainCarHeight);
-        //constrain;
     }
-
-    //public List<float> GetYCoordinateList()
-    //{
-    //    return _yCoordinateList;
-    //}
-
-    //public void SetYCoordinateList(List<float> coordinateList)
-    //{
-    //    _yCoordinateList = coordinateList;
-    //}
 }
