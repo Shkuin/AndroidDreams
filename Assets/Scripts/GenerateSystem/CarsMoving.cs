3695c5cf4c0f3331639d9fc426aa33b8f553dd41
Shkuin <57735819+Shkuin@users.noreply.github.com>
2022-10-06 11:25:00 -0700

    Some refactoring
    refactored some code and moved CarGenerated System in VariableBackground Scene

--- Assets/Scripts/GenerateSystem/CarsMoving.cs
+++ Assets/Scripts/GenerateSystem/CarsMoving.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightMoveCars : MonoBehaviour
+public class CarsMoving : MonoBehaviour
 {
     public float speed = -3f;
     void Update()
